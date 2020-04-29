991f2ad77b2825bf97ee244dc42ca4f4c196e094
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-04-29 03:01:49 +0000

    Done.

--- app.rb
+++ app.rb
@@ -3,7 +3,13 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
-    "Welcome to your app!!!! "
+    "Started my server using Shotgun!"
   end
+  #this is the controller action
+  
+  #localhost:9292 is the local port, in Rack we use this
+  #localhost:9393 is the default shotgun port 
+  
+  #A port is just an endpoint on the server that is open for communication. It's typical for a server to regulate the open ports to make sure it can monitor requests appropriately.
 
 end
